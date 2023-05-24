56232f550640caf59a4543a67a0cca8e5585c210
sadaf-Daneshgar <sadaf.barekzai54321@gmail.com>
2023-05-24 20:43:11 +0430

    Add new changes

--- script.js
+++ script.js
@@ -36,3 +36,18 @@ projectCard.forEach((card) => {
 menubtn.addEventListener('click', () => openMenu());
 closeBtn.addEventListener('click', () => closeMenu());
 closebtn.addEventListener('click', () => closeModalDesc());
+
+// validation part
+const email = document.getElementById('email');
+const form = document.getElementById('form');
+const message = document.querySelector('.message');
+
+form.addEventListener('submit', (e) => {
+  const regex = /[A-Z]/;
+  if (regex.test(email.value)) {
+    e.preventDefault();
+    message.style.opacity = 1;
+  } else {
+    message.style.opacity = 0;
+  }
+});
